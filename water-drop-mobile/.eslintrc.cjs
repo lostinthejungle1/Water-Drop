6e6cb664934660bb227ad5a9719065e971a3971e
Liang <lianginchina1999@gmail.com>
2024-04-06 13:24:48 +0800

    set up apollo graphql client

--- water-drop-mobile/.eslintrc.cjs
+++ water-drop-mobile/.eslintrc.cjs
@@ -9,7 +9,8 @@ module.exports = {
     'react/function-component-definition': 0,
     'react/react-in-jsx-scope': 0,
     'import/prefer-default-export': 0,
-    'react-hooks/exhaustive-deps': 1
+    'react-hooks/exhaustive-deps': 1,
+    'indent': 'off'
   },
   parser: require.resolve('@typescript-eslint/parser'),
   parserOptions: {
--- water-drop-mobile/src/main.tsx
+++ water-drop-mobile/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ApolloProvider } from '@apollo/client';
+import App from './App.tsx';
+import './index.css';
+import { client } from './utils/apollo.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-)
+  </ApolloProvider>,
+);
